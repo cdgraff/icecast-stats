ae3dd0c916c4928f0a5e1ea2ad5aafc099c9519e
Alejandro Ferrari <cdgraff@gmail.com>
2012-06-14 11:38:31 -0300

    Add Sent Bytes update field

--- listener_leave.php
+++ listener_leave.php
@@ -5,7 +5,7 @@ include('db.php');
 $post = print_r($_POST, true);
 
 
-$query = 'UPDATE icecast SET duration = '. $_POST['duration'] .', datetime_end = NOW() WHERE icecast_id = '.$_POST['client'].' ORDER BY id DESC LIMIT 1';
+$query = 'UPDATE icecast SET duration = '. $_POST['duration'] .', sent_bytes = '. $_POST['sent'] .', datetime_end = NOW() WHERE icecast_id = '.$_POST['client'].' ORDER BY id DESC LIMIT 1';
 
 echo $query;
 
