c6e6a6cb0a347b7cf581074b6b75e3daddcdb01b
Alejandro Ferrari <cdgraff@gmail.com>
2012-06-14 11:34:20 -0300

    Add referrer new field Only available into the latest icecat branch (14/06/2012)

--- listener_join.php
+++ listener_join.php
@@ -6,7 +6,7 @@ include('db.php');
 
 $post = print_r($_POST, true);
 
-$query = 'INSERT into icecast (icecast_id, ip, mount, agent, server, port, datetime_start) VALUES ('. $_POST['client'] .', \'' .  $_POST['ip']  . '\', \'' . $_POST['mount']  .'\', \''. $_POST['agent'] .'\', \'' . $_POST['server'] .'\', ' . $_POST['port'] .', NOW())';
+$query = 'INSERT into icecast (icecast_id, ip, mount, agent, referrer, server, port, datetime_start) VALUES ('. $_POST['client'] .', \'' .  $_POST['ip']  . '\', \'' . $_POST['mount']  .'\', \''. $_POST['agent'] .'\', \''. $_POST['referrer'] .'\', \'' . $_POST['server'] .'\', ' . $_POST['port'] .', NOW())';
 
 echo $query;
 
